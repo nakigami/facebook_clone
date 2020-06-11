72f89b2daca00e2857e37334efbdc3c03ee12d50
Anas RIANI <anas.devriani@gmail.com>
2020-06-11 17:55:14 +0100

    Creation of RetrievePostsTest to test posts fetching

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\Post as PostResource;
+use App\Http\Resources\PostCollection;
 use App\Post;
-use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
+    // Retrieve all posts using PostCollection
+    public function index()
+    {
+        return new PostCollection( Post::all() );
+    }
+
+    // Store one Post item
     public function store()
     {
         $data = \request()->validate([
---
+++ tests/Feature/RetrievePostsTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Post;
+use App\User;
+use Tests\TestCase;
+
+class RetrievePostsTest extends TestCase
+{
+    /**
+     * @test
+     *
+     */
+    public function a_user_can_retrieve_posts()
+    {
+        $this->withoutExceptionHandling();
+        $this->actingAs($user = factory(User::class)->create(),'api');
+
+        $posts = factory(Post::class, 2)->create();
+
+        $response = $this->get('/api/posts');
+        $response->assertStatus(200)
+            ->assertJson([
+                'data' => [
+                    [
+                        'data' => [
+                            'type' => 'posts',
+                            'post_id' => $posts->first()->id,
+                            'attributes' => [
+                                'body' => $posts->first()->body
+                            ]
+                        ],
+                    ],
+                    [
+                        'data' => [
+                            'type' => 'posts',
+                            'post_id' => $posts->last()->id,
+                            'attributes' => [
+                                'body' => $posts->last()->body
+                            ]
+                        ],
+                    ]
+                ],
+                'links' => [
+                    'self' => url('/posts')
+                ]
+            ]);
+    }
+}
