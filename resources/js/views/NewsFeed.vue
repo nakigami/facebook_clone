b9c631c9572a516d76976ba80581dfddf7d750c5
Anas RIANI <anas.devriani@gmail.com>
2020-06-12 23:57:14 +0100

    Creatioon of Post Vue Component

--- resources/js/views/NewsFeed.vue
+++ resources/js/views/NewsFeed.vue
@@ -1,14 +1,16 @@
 <template>
     <div class="flex flex-col items-center py-4">
         <NewPost />
+        <Post />
     </div>
 </template>
 
 <script>
     import NewPost from "../components/NewPost";
+    import Post from "../components/Post";
     export default {
         name: "NewsFeed",
-        components: {NewPost}
+        components: {Post, NewPost}
     }
 </script>
 
